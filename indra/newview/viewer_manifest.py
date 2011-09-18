030817a0f8841d75032bd5757a076f4000f3031c
Ansariel <none@none>
2011-09-18 14:18:58 +0200

    Updated tcmalloc for Linux - by Henri Beauchamp (Cool VL)

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -960,7 +960,7 @@ class Linux_i686Manifest(LinuxManifest):
 #            self.path("libopenal.so", "libvivoxoal.so.1") # vivox's sdk expects this soname
             self.path("libtcmalloc.so", "libtcmalloc.so") #formerly called google perf tools
             self.path("libtcmalloc.so.0", "libtcmalloc.so.0") #formerly called google perf tools
-            self.path("libtcmalloc.so.0.1.0", "libtcmalloc.so.0.1.0") #formerly called google perf tools
+            self.path("libtcmalloc.so.0.2.2", "libtcmalloc.so.0.2.2") #formerly called google perf tools
             self.path("libcollada14dom.so", "libcollada14dom.so") # Mesh support
             self.path("libminizip.so", "libminizip.so") # Mesh support
             self.path("libglod.so", "libglod.so") # Mesh support
