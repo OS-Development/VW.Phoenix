0b5bfc2dcfbf2aa3a799e553bcb8a114eff68b0a
Tank_Master <none@none>
2012-02-23 23:24:37 -0800

    turn off packaging in config comand its erroring atm, will need to figure out why

--- indra/config.bat
+++ indra/config.bat
@@ -1,4 +1,4 @@
 @echo
 set lib=%lib%
 set include=%include%
-develop.py -G vc100 -tRelease configure -DUSE_PRECOMPILED_HEADERS:BOOL=ON -DLL_TESTS=OFF -DPACKAGE:BOOL=TRUE  2>&1 |c:\cygwin\bin\tee Config.log
+python develop.py -G vc100 -tRelease configure -DLAA:BOOL=ON -DUSE_PRECOMPILED_HEADERS:BOOL=ON -DLL_TESTS=OFF -DPACKAGE:BOOL=OFF  2>&1 |c:\cygwin\bin\tee Config.log
